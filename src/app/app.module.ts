dc957720d9f3bdc5883afd65f059e0ac281e3d1a
santy <santyvegarajr@gmail.com>
2022-05-12 19:55:22 +0200

    pantalla-inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PantallaInicialComponent } from './pantalla-inicial/pantalla-inicial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PantallaInicialComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pantalla-inicial/pantalla-inicial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pantalla-inicial',
+  templateUrl: './pantalla-inicial.component.html',
+  styleUrls: ['./pantalla-inicial.component.scss']
+})
+export class PantallaInicialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
