0f76da1d87fff0cc2a7a955d7c9707d699afd06a
santy <santyvegarajr@gmail.com>
2022-05-23 19:53:34 +0200

    principio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PantallaInicialComponent } from './pantalla-inicial/pantalla-inicial.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PantallaInicialComponent
+    PantallaInicialComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
