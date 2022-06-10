6cfa613cb185e383b57cf12d261ea6b4fd594557
francisco <franciscoballesta4@gmail.com>
2022-06-10 11:27:40 +0200

    botones

--- src/app/pagina-cursos/pagina-cursos.component.ts
+++ src/app/pagina-cursos/pagina-cursos.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginaCursosComponent implements OnInit {
 
+  public IsMostrarCursosPresencial: boolean = false
+  public IsMostrarCursosOnline: boolean = false
+  public btnTextPresencial: string = ""
+  public btnTextOnline: string = ""
+  private textCursosPresencialShow: string = "Mostrar Cursos Presenciales"
+  private textCursosPresencialHide: string = "Ocultar Precios Presenciales"
+  private textCursosOnlineShow: string = "Mostrar Cursos Online"
+  private textCursosOnlineHide: string = "Ocultar Precios Online"
+
   constructor() { }
 
+  public toggleCursoPresencial():void{
+    this.IsMostrarCursosPresencial = !this.IsMostrarCursosPresencial
+    this.checkCursoPresencial()
+  }
+
+  public toggleCursoOnine():void{
+    this.IsMostrarCursosOnline = !this.IsMostrarCursosOnline
+    this.checkCursoOnline()
+  }
+
+  private checkCursoPresencial():void{
+    if(this.IsMostrarCursosPresencial){
+      this.btnTextPresencial = this.textCursosPresencialHide
+    }else{
+      this.btnTextPresencial = this.textCursosPresencialShow
+    }
+  }
+
+  private checkCursoOnline():void{
+    if(this.IsMostrarCursosOnline){
+      this.btnTextOnline = this.textCursosOnlineHide
+    }else{
+      this.btnTextOnline = this.textCursosOnlineShow
+    }
+  }
+
   ngOnInit(): void {
+    this.checkCursoPresencial()
+    this.checkCursoOnline()
   }
 
 }
